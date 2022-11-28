ef4ddd15cf6696e73656c7b8c64ae8977ee1a88f
이진우 <76528931+dlehgus97@users.noreply.github.com>
2022-11-28 16:05:24 +0900

    Create 8958 OX퀴즈.java

---
+++ BaekJoon/8958 OX퀴즈.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[]args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[N];
+
+        for(int i = 0 ; i<N; i++){
+            arr[i] = br.readLine();
+        }
+
+        for(int i = 0; i<N; i++){
+            int count = 0;
+            int result = 0;
+            for(int j = 0; j<arr[i].length(); j++){
+                if (arr[i].charAt(j) == 'O'){
+                    count ++;
+                }
+                else
+                    count = 0 ;
+                result += count;
+            }
+System.out.println(result);
+        }
+    }
+}
