2d466d0427eb25ec8adadf27ea9d14d85d0a96bc
이진우 <76528931+dlehgus97@users.noreply.github.com>
2022-12-04 00:07:50 +0900

    Update 2738 행렬 덧셈.java

--- BaekJoon/2738 행렬 덧셈.java
+++ BaekJoon/2738 행렬 덧셈.java
@@ -34,3 +34,36 @@ public class Main {
 
     }
 }
+
+//3중 행렬로 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void  main(String []args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int [][][] arr = new int[2][N][M];
+        for(int k = 0; k<2; k++) {
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st1 = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M; j++) {
+                    arr[k][i][j] = Integer.parseInt(st1.nextToken());
+                }
+            }
+        }
+
+        for(int i = 0 ; i<N; i++){
+            for(int j = 0 ; j<M; j++){
+                System.out.print(arr[0][i][j] +arr[1][i][j] + " ");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
