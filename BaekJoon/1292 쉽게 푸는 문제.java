a88f6f95840820aabb827c5a7ad989bf85ca5ef4
이진우 <76528931+dlehgus97@users.noreply.github.com>
2022-12-30 12:34:27 +0900

    Update 1292 쉽게 푸는 문제.java

--- BaekJoon/1292 쉽게 푸는 문제.java
+++ BaekJoon/1292 쉽게 푸는 문제.java
@@ -1,37 +1,31 @@
-mport java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_1292 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		
-		int[] nums = new int[1000];
-		
-		int cnt = 0;
-		int check = 1;
-		
-		for(int i = 0 ; i < 1000 ; i++) {
-			nums[i] = check;
-			cnt++;
-			
-			if(check == cnt) {
-				check++;
-				cnt = 0;
-			}
-		}
-		
-		int sum = 0;
-		
-		for(int i = a ; i <= b ; i++) {
-			sum += nums[i-1];
-		}
-		
-		System.out.println(sum);
-	}
+
+public class Main {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int result = 0 ;
+        for(int i = 1; i<=B; i++){
+            result +=i;
+        }
+        int [] arr = new int[result];
+        int count = 0;
+        for(int i = 1 ; i<=B; i++){
+            for(int j = 0 ; j<i; j++) {
+                arr[count] = i;
+                count++;
+            }
+        }
+        int num = 0;
+        for(int i = A-1 ; i<=B-1; i++){
+            num += arr[i];
+        }
+        System.out.println(num);
+    }
 }
