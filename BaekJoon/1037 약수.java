64b912a553c684687b82dc1202081315a5a07d76
이진우 <76528931+dlehgus97@users.noreply.github.com>
2023-01-08 22:27:17 +0900

    Update 1037 약수.java

--- BaekJoon/1037 약수.java
+++ BaekJoon/1037 약수.java
@@ -1,27 +1,23 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		int max = Integer.MIN_VALUE;
-		int min = Integer.MAX_VALUE;
-		
-		StringTokenizer st = new StringTokenizer(br.readLine()," ");
-        
-		while(T-- > 0) {
-			int N = Integer.parseInt(st.nextToken());
-			max = N > max ? N : max;
-			min = N < min ? N : min;
-		}
-		System.out.println(max * min);
-	}
- 
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0 ; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+	    
+        Arrays.sort(arr);
+
+        System.out.println(arr[0] * arr[N-1]);
+
+     }
 }
