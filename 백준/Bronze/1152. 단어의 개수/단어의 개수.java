1b79eee6288a89dce8f7c3269a8c0b43fe241d38
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-01-05 23:59:28 +0900

    Update 단어의 개수.java

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,3 +1,27 @@
+//method_1
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String word = br.readLine();
+        StringTokenizer st = new StringTokenizer(word);
+
+        int result = 0 ;
+        while(st.hasMoreTokens()){
+            String tmp = st.nextToken();
+            result ++;
+        }
+        System.out.println(result);
+    }
+}
+
+//method_2
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
