c228b0ad4af5b4fcba886f093933ddebce8c9c7f
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-01-14 01:32:50 +0900

    [Silver V] Title: 나이순 정렬, Time: 780 ms, Memory: 48760 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String [][] arr = new String [N][2];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 2; j++) {
+                arr[i][j] = st.nextToken();
+            }
+        }
+
+        Arrays.sort(arr, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                if(o1[0] == o2[0]){
+                    return o1[1].compareTo(o2[1]);
+                }
+                else{
+                    return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+                }
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
