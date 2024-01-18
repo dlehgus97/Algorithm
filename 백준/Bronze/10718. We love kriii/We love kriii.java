71c88d4976ed5922931c114a465851550461e494
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-01-18 19:22:49 +0900

    [Bronze V] Title: We love kriii, Time: 112 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String [] args) throws IOException{
+        StringBuilder sb = new StringBuilder();
+        sb.append("강한친구 대한육군").append("\n").append("강한친구 대한육군");
+        System.out.println(sb);
+
+    }
+}
