2222cb0e96cafc9320a22a80bd31b336e2f11eba
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-02-17 00:26:25 +0900

    [level 1] Title: 예산, Time: 0.33 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        
+        Arrays.sort(d);
+        int total = 0;
+        
+        for(int i = 0; i<d.length; i++){
+            if(total +d[i] > budget){
+                break;
+            }
+            else{
+                total += d[i];
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
