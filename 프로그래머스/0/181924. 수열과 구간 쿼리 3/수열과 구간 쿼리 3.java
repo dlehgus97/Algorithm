7b0009f701801600d18b01a770d4ae12ede04e82
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-02-19 21:18:23 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.06 ms, Memory: 90.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        int[] answer = new int [arr.length];
+        
+        for(int i = 0 ; i<queries.length; i++){
+            int tmp = 0;
+            tmp = arr[queries[i][0]] ;
+            arr[queries[i][0]] = arr[queries[i][1]] ;
+            arr[queries[i][1]] = tmp;
+        }
+        answer = arr;
+       
+        return answer;
+    }
+}
