a73c9348d909f6423accdfedfeee441bd82a424b
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-02-26 21:04:31 +0900

    [Bronze II] Title: 나누기, Time: 120 ms, Memory: 14248 KB -BaekjoonHub

---
+++ 백준/Bronze/1075. 나누기/나누기.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int F = Integer.parseInt(br.readLine());
+
+        N = (N/100) * 100;
+
+        while(true){
+            if(N % F == 0){
+                break;
+            }
+            N++;
+        }
+        int result = N % 100;
+
+        StringBuilder sb = new StringBuilder();
+        if(result<10){
+           sb.append(0).append(result);
+        }
+        else{
+            sb.append(result);
+        }
+        System.out.println(sb);
+    }
+}
