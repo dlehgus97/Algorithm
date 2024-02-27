a800f6780259eb84510aeb410828cca0826c3248
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-02-27 20:22:24 +0900

    [level 0] Title: 할 일 목록, Time: 0.02 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        int count = 0;
+        
+        for(int i = 0 ; i<finished.length; i++){
+            if(!finished[i]){
+                count++;
+            }
+        }
+        
+        String[] answer = new String[count];
+        
+        int result = 0;
+        for(int i = 0 ; i<todo_list.length ; i++){
+            if(!finished[i]){
+                answer[result] = todo_list[i];
+                result++;
+            }
+        }
+        return answer;
+    }
+}
