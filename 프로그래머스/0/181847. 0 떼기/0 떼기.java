4b80582860d5c18433abea4499e159582752338c
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-02-28 17:06:17 +0900

    [level 0] Title: 0 떼기, Time: 11.07 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String n_str) {
+        String answer = "";
+        
+        int tmp = 0;
+        
+        for(int i = 0 ; i<n_str.length(); i++){
+            if(n_str.charAt(i) != '0'){
+                tmp = i;
+                break;
+            }
+        }
+        
+        for(int i = tmp; i<n_str.length(); i++){
+            answer +=n_str.charAt(i);
+        }
+        
+        return answer;
+    }
+}
