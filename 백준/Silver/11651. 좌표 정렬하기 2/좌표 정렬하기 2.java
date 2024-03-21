96d9603883e4812b23e645b55ab570107ebc861e
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-03-21 14:40:39 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 696 ms, Memory: 49648 KB -BaekjoonHub

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -2,34 +2,41 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
+
         int[][] arr = new int[N][2];
+        StringTokenizer st;
 
         for (int i = 0; i < N; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < 2; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
-            }
+            st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
         }
-
-        Arrays.sort(arr, (arr_1, arr_2) -> {
-            if (arr_1[1] == arr_2[1]) {
-                return arr_1[0] - arr_2[0];
-            } else {
-                return arr_1[1] - arr_2[1];
+        
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[1] == o2[1]) {
+                    return o1[0] - o2[0];
+                } else
+                    return o1[1] - o2[1];
             }
         });
 
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
-            sb.append(arr[i][0]).append(" ").append(arr[i][1]).append("\n");
+        for (int i = 0; i < arr.length; i++) {
+            sb.append(arr[i][0]).append(" ").append(arr[i][1]);
+            sb.append("\n");
         }
-        System.out.print(sb);
+
+        System.out.println(sb.substring(0, sb.length()));
     }
 }
