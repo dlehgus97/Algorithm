bd0b1d6137224837d36e727f41c96e8d5ea4045b
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-03-22 12:20:24 +0900

    [level 0] Title: 겹치는 선분의 길이, Time: 0.04 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/0/120876. 겹치는 선분의 길이/README.md
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/README.md
@@ -1,10 +1,10 @@
 # [level 0] 겹치는 선분의 길이 - 120876 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120876) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120876#) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.02 ms
+메모리: 70.8 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 12:17:35
+2024년 03월 22일 12:20:23
 
 ### 문제 설명
 
--- 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
+++ 프로그래머스/0/120876. 겹치는 선분의 길이/겹치는 선분의 길이.java
@@ -5,11 +5,13 @@ class Solution {
         int [] arr = new int [201];
         
         for(int i = 0 ; i<lines.length; i++){
-                
-            for(int j = lines[i][0] + 1  ; j<=lines[i][1]; j++){
+            for(int j = lines[i][0]; j<lines[i][1]; j++){ 
                 arr[j + 100] += 1;
             }
         }
+        
+        //0 ~ 2 선분이라면 0 ~ 1 선분을 arr[101]++; 해서 좌표로 보는게 아니라 선분으로 봤다.
+        //그래서 선분이 2개 이상이라면 겹치는걸로 처리했다 .
         for(int i = 0 ; i<arr.length; i++){
             if(arr[i] > 1){
                 answer++;
