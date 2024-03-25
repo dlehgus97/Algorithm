79318f2eec57f64ea9f21d477e149e6351756661
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-03-25 09:31:46 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.08 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
+++ 프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.java
@@ -1,35 +1,33 @@
 class Solution {
     public int[][] solution(int n) {
-        int[][] answer = new int[n][n];
-        boolean[][] check = new boolean[n][n];
+        int[][] answer = new int [n][n];
         
-        int[] dy = {0, 1, 0, -1};
-        int[] dx = {1, 0, -1, 0};
+        boolean [][] check = new boolean[n][n];
+        
+        int [] dx = {1,0,-1,0};
+        int [] dy = {0,1,0,-1};
         
         int result = 1;
-        
-        int x = 0;
+        int x = 0 ;
         int y = 0;
-        int dir = 0; 
-      
-        for(int i = 0 ; i< n * n ; i++){
+        int dir = 0;
+        
+        for(int i = 0 ; i<n*n; i++){
             answer[y][x] = result;
-            check[y][x] = true;
+            check[y][x] =true;
             
             int nx = x + dx[dir];
             int ny = y + dy[dir];
-            
-             if (nx < 0 || nx >= n || ny < 0 || ny >= n || check[ny][nx]) {
+            if(nx < 0 || nx >= n || ny < 0 || ny >= n || check[ny][nx] ){
                 dir = (dir + 1) % 4;
                 nx = x + dx[dir];
                 ny = y + dy[dir];
             }
-            
             x = nx;
             y = ny;
             result++;
-            
         }
+        
         return answer;
     }
 }
