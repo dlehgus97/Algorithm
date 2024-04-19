983872f4da02b0d52c0decde5a54ce731b7167fa
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-04-19 11:34:32 +0900

    [Gold III] Title: 소수의 연속합, Time: 1960 ms, Memory: 29688 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,61 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
-    static ArrayList<Integer>list ;
-
-    public static void main(String [] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
 
-        list = new ArrayList<>();
+        int[] primes = new int[N + 1];
+        int index = 0;
+        for (int i = 2; i <= N; i++) {
+            if (isPrime(i)) {
+                primes[index++] = i;
+            }
+        }
 
-        isPrime(N);
-
-        list.add(0); // 끝에 더 못 가게 어차피 여기보다 작으면 답이 안나옴
-
-        int start = 0 ;
+        int start = 0;
         int end = 0;
 
-        int total = 0 ;
-        int result = 0 ;
+        int total = 0;
+        int result = 0;
 
-        StringBuilder sb = new StringBuilder();
-        while(end != list.size()){
-            if(total<N){
-                total += list.get(end);
-                end++;
-            }
-            else if(total > N){
-                total -= list.get(start);
-                start++;
-            }
-            else{
-                total += list.get(end);
-                end ++;
+        while (end <= index) {
+            if (total < N) {
+                total += primes[end++];
+            } else if (total > N) {
+                total -= primes[start++];
+            } else {
+                total += primes[end++];
                 result++;
             }
         }
-        sb.append(result);
+
         System.out.println(result);
     }
 
-    public static void isPrime(int a){
-
-        for (int i = 2; i <= a; i++) {
-            boolean check = true;
-            for (int j = 2; j * j <= i; j++) {
-                if (i % j == 0) {
-                    check = false;
-                    break;
-                }
-            }
-            if (check) {
-                list.add(i);
-            }
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++) {
+            if (num % i == 0) return false;
         }
+        return true;
     }
 }
