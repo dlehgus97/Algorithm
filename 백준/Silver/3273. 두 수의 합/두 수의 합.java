d617427e1ed50c85126dd4ba13f7ee2572f43c15
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-04-19 10:24:36 +0900

    [Silver III] Title: 두 수의 합, Time: 352 ms, Memory: 25892 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,33 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.nio.Buffer;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main{
-    public static void main(String [] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int [] arr = new int[N];
-        for(int i = 0 ; i<arr.length; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        int X = Integer.parseInt(br.readLine());
+        int x = Integer.parseInt(br.readLine());
 
+        Arrays.sort(arr);
 
+        int left = 0;
+        int right = arr.length - 1;
         int result = 0;
-        for(int i = 0 ; i<arr.length - 1; i++){
-            for(int j = i+1; j<arr.length ; j++){
-                if(X - arr[i] == arr[j]){
-                    result ++;
-                    break;
-                }
+
+        while (left < right) {
+            int tmp = arr[left] + arr[right];
+            if (tmp < x) {
+                left++;
+            } else if (tmp > x) {
+                right--;
+            } else {
+                result++;
+                left++; 
+                right--; 
             }
         }
+
         System.out.println(result);
     }
 }
