fa973d269304b10af885c8fcf10d25cf0d38c1fe
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-04-23 09:48:48 +0900

    [Bronze III] Title: 전자레인지, Time: 144 ms, Memory: 15880 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/10162. 전자레인지/전자레인지.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = {300, 60, 10};
+
+        StringBuilder sb = new StringBuilder();
+
+        if (N % 10 != 0) {
+            sb.append(-1);
+        } else {
+            for (int i = 0; i < 3; i++) {
+                int result = 0;
+
+                result += N / arr[i];
+
+                sb.append(result + " ");
+                N %= arr[i];
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
