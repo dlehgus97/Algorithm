6ea4dc001c7092a458efd6ca3f8f23b710217c10
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-04-23 09:30:35 +0900

    [Gold V] Title: 강의실 배정, Time: 696 ms, Memory: 68832 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[][] arr = new int[N][2];
+        boolean[] check = new boolean[N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                }
+                return o1[0] - o2[0];
+            }
+        });
+        PriorityQueue<Integer> pq = new PriorityQueue<>();  // 종료 시간을 기준으로 자동정렬 하기위해 사용한다 .
+
+        pq.add(arr[0][1]);   // 기준이 되는 첫번째 시간
+
+        for (int i = 1; i < N; i++) {
+            if (pq.peek() <= arr[i][0]) {       // 정렬된 시작 순서가 종료된 시간보다 크면 제거해준다
+                pq.poll();
+            }
+            pq.add(arr[i][1]);      //계속해서 추가해준다 (위에서 제거 해주기때문에 추가가 가능한것)
+        }
+        System.out.println(pq.size());
+    }
+}
