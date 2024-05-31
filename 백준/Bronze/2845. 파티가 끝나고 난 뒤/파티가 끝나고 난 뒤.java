7418420411f4887ab90c4f36e956a16631124253
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-05-31 17:44:18 +0900

    [Bronze IV] Title: 파티가 끝나고 난 뒤, Time: 100 ms, Memory: 14012 KB -BaekjoonHub

---
+++ 백준/Bronze/2845. 파티가 끝나고 난 뒤/파티가 끝나고 난 뒤.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < 5; i++) {
+            int tmp = Integer.parseInt(st.nextToken());
+            sb.append(tmp - (N * M)).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
