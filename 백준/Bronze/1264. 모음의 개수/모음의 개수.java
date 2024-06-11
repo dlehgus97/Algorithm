1df011982f622534efab3090cee1a61081911ae0
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-06-11 22:19:35 +0900

    [Bronze IV] Title: 모음의 개수, Time: 108 ms, Memory: 15356 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            String str = br.readLine();
+
+            if (str.equals("#")) {
+                break;
+            }
+            str = str.replaceAll("[aeiouAEIOU]", "-");
+
+            int result = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '-') {
+                    result++;
+                }
+            }
+            sb.append(result).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
