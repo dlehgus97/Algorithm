7e2a4600a52d6111c0861b1e95b7ccab644f8260
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-07-09 00:36:12 +0900

    [level 1] Title: K번째수, Time: 0.20 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,19 +1,16 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int [commands.length];
         
         for(int i = 0 ; i<commands.length; i++){
-            int [] arr = new int [commands[i][1] - commands[i][0] + 1];
-            int count = 0 ;
-            for(int j = commands[i][0] -1 ; j<commands[i][1]; j++){
-                arr[count] = array[j];
-                count++;
+            ArrayList<Integer> list = new ArrayList<>();
+            for(int j = commands[i][0] - 1; j < commands[i][1]; j++){
+                 list.add(array[j]);
             }
-            Arrays.sort(arr);
-            answer[i] = arr[commands[i][2] - 1];
+            Collections.sort(list);
+            answer[i] = list.get(commands[i][2] -1 );
         }
         return answer;
     }
