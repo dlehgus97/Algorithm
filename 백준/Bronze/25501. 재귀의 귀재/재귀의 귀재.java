dd54ec48d3d62eabcd8b18a4f2f9ac6e3ee3b751
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-07-08 23:50:08 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 236 ms, Memory: 19280 KB -BaekjoonHub

--- 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -3,7 +3,19 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    static int result;
+    public static int result;
+
+    public static int recursion(String s, int l, int r) {
+        result++;
+        if (l >= r) return 1;
+        else if (s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l + 1, r - 1);
+    }
+
+    public static int isPalindrome(String s) {
+        return recursion(s, 0, s.length() - 1);
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -12,25 +24,10 @@ public class Main {
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < N; i++) {
-            String word = br.readLine();
+            String str = br.readLine();
             result = 0;
-            sb.append(isPalindrome(word) + " ").append(result).append("\n");
+            sb.append(isPalindrome(str)).append(" ").append(result).append("\n");
         }
         System.out.println(sb);
-    }
-
-    public static int isPalindrome(String word) {
-        return recursion(word,0,word.length() -1);
-    }
-
-    public static int recursion(String word, int l, int r) {
-        result ++;
-        if (l >= r) {
-            return 1;
-        } else if (word.charAt(l) != word.charAt(r)) {
-            return 0;
-        } else {
-            return recursion(word,l+1,r-1);
-        }
     }
 }
