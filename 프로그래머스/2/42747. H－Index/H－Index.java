79a9e6a30ddece495913429a385938928e295f97
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-07-09 09:32:31 +0900

    [level 2] Title: H-Index, Time: 0.47 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,4 +1,3 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
@@ -8,10 +7,10 @@ class Solution {
         Arrays.sort(citations);
         
         //1,2,3,6,8,10,12,15
-        //num이 citations 요솟값보다 커지면  
-        for(int i = 0  ;i<citations.length; i++){
+        //num이 citations 요솟값보다 커지면
+        for(int i = 0 ; i<citations.length ; i++){
             int num = citations.length - i;
-            if(citations[i] >= num){
+            if(citations[i] >=num){
                 answer = num;
                 break;
             }
