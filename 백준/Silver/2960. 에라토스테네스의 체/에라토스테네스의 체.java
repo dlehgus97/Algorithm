1ae275db83fcd5f01c698809c552b23403a532f8
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-07-18 20:29:04 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 132 ms, Memory: 14184 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -8,27 +8,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        ArrayList<Integer> arr1 = new ArrayList<>();
-        ArrayList<Integer> arr2 = new ArrayList<>();
-        int result = 0;
+        ArrayList<Integer> list = new ArrayList<>();
 
         for (int i = 2; i <= N; i++) {
-            arr1.add(i);
+            list.add(i);
         }
 
+        int count = 0;
         for (int i = 2; i <=N; i++) {
-            for (int j = 0; j < arr1.size(); j++) {
-                if(arr1.get(j) % i == 0){
-                    result ++;
-                    if(result == K){
-                        System.out.println(arr1.get(j));
+            for (int j = 0; j < list.size(); j++) {
+                if (list.get(j) % i == 0) {
+                    count++;
+                    if (count == K) {
+                        System.out.print(list.get(j));
                         break;
                     }
-                    arr1.remove(j);
+                    list.remove(j);
                 }
             }
         }
