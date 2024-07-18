9bce3b763d957a4b9671a7a7555c15e485679455
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-07-18 19:48:35 +0900

    [Bronze I] Title: 최소공배수, Time: 144 ms, Memory: 14532 KB -BaekjoonHub

--- 백준/Bronze/1934. 최소공배수/최소공배수.java
+++ 백준/Bronze/1934. 최소공배수/최소공배수.java
@@ -4,24 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String [] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
-
-        for(int i = 0 ; i<T; i++){
+        for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int A = Integer.parseInt(st.nextToken());
-            int B = Integer.parseInt(st.nextToken());
-            sb.append((A*B)/gdc(A,B)).append("\n");
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int result = gcd(a, b);
+            sb.append(a * b / result).append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
-    public static int gdc(int a , int b){
-        if(b == 0){
+    
+    public static int gcd(int a, int b) {
+        if (b == 0) {
             return a;
         }
-        return gdc(b,a%b);
+        return gcd(b, a % b);
     }
 }
