76e23b730e236bd53dbe1ce51ac64dcad0ff8e93
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-07-18 12:49:47 +0900

    [level 2] Title: 카펫, Time: 0.09 ms, Memory: 72.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,21 +1,24 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
         
-        int total = brown + yellow;
+        int sum = brown + yellow;
         
-        for (int i = 1; i <= total / 2; i++) {
-            if (total % i == 0) {
+        ArrayList<Integer>list = new ArrayList<>();
+        
+        for(int i = 1 ; i<=Math.sqrt(sum); i++){
+            if(sum % i == 0){
                 int width = i;
-                int height = total / i;
-                if ((width - 2) * (height - 2) == yellow) {
+                int height = sum / i;
+                if((height -2) * (width - 2) == yellow){
                     answer[0] = height;
                     answer[1] = width;
                     break;
-                }
+                } 
             }
-        }
-        
+        } 
         return answer;
     }
 }
