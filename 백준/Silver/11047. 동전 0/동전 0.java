12979f48aa361b29bcbd5aebd57f0ea419f79d2b
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-07-18 19:42:33 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -4,28 +4,26 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String [] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        int [] arr = new int[N];
+        int[] arr = new int[N];
 
-        for(int i = 0 ; i<N; i++){
+        for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        int result = 0 ;
+        int cnt = 0;
 
-        for(int i = arr.length -1 ; i>=0; i--){
-            if(K / arr[i] >0){
-                result += K / arr[i];
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (K >= arr[i]) {
+                cnt += K / arr[i];
                 K %= arr[i];
             }
         }
-        System.out.println(result);
-
+        System.out.print(cnt);
     }
 }
