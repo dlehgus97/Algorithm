aa6c9d7e9dc319449b8952b4176c52ad30587643
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-07-22 14:56:07 +0900

    [Silver III] Title: 소수 구하기, Time: 476 ms, Memory: 18816 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -3,27 +3,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        StringBuilder sb = new StringBuilder();
-
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        for(int i = N ; i<=M ; i++){
-            if(prime(i) == true){
+        StringBuilder sb = new StringBuilder();
+        for (int i = N; i <= M; i++) {
+            if (prime(i)) {
                 sb.append(i).append("\n");
             }
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
-    public static boolean prime(int num){
-        if(num == 1) return false;
-        for(int i = 2; i*i<=num; i++){  // 약수를 절반만 사용한다고 생각하면 된다
-            if(num % i == 0) return false;
+
+    public static boolean prime(int a) {
+        if (a < 2) {
+            return false;
+        }
+        for (int i = 2; i * i <= a; i++) {
+            if (a % i == 0) {
+                return false;
+            }
         }
         return true;
     }
