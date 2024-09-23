f8c3b6e862865cbad3bb68d6ef487248aae57b8c
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-09-23 18:59:39 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 1.67 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.java
@@ -3,15 +3,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] numbers) {
         
-        Arrays.sort(numbers);
         HashSet<Integer>set = new HashSet<>();
         
-        for(int i = 0 ; i<numbers.length -1 ; i++){
-            for(int j = i+1; j<numbers.length; j++){
-                int result = numbers[i] + numbers[j];
-                set.add(result);
-            }
-        }
+        
+       for(int i = 0 ; i<numbers.length - 1; i++){
+           for(int j = i+1 ; j<numbers.length; j++){
+               int num = numbers[i] + numbers[j];
+               set.add(num);
+           }
+       } 
         
         int [] answer = new int [set.size()];
         
@@ -19,38 +19,10 @@ class Solution {
         
         Collections.sort(list);
         
-        for(int i = 0  ;i<answer.length; i++){
+        for(int i = 0 ; i<answer.length; i++){
             answer[i] = list.get(i);
         }
-
+        
         return answer;
     }
 }
-
-// set은 정렬이 자동으로 안되는 걸 까먹음 운좋게 테스트케이스가 정렬되는 문제였다.
-// import java.util.*;
-
-// class Solution {
-//     public int[] solution(int[] numbers) {
-        
-//         Arrays.sort(numbers);
-//         HashSet<Integer>set = new HashSet<>();
-        
-//         for(int i = 0 ; i<numbers.length -1 ; i++){
-//             for(int j = i+1; j<numbers.length; j++){
-//                 int result = numbers[i] + numbers[j];
-//                 set.add(result);
-//             }
-//         }
-        
-//         int [] answer = new int [set.size()];
-        
-//         Iterator<Integer> ite = set.iterator();
-        
-//         for(int i = 0  ;i<answer.length; i++){
-//             answer[i] = ite.next();
-//         }
-
-//         return answer;
-//     }
-// }
