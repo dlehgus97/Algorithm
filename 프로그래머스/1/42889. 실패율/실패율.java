7183606672bc05cc2449069d289307de37b2bc33
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-09-24 16:34:48 +0900

    [level 1] Title: 실패율, Time: 171.92 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,50 +1,46 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] answer = new int [N];
-        double [] arr = new double[N];
-        int [] num = new int [N];
+        int[] answer = new int[N];
+        double [] arr = new double[N]; // 실패율 담을 배열 
+        int [] num = new int[N];
         double [] copy = new double[N];
         
-        int len = stages.length;
+        
+        
+        int len = stages.length; // 줄어들어야 하기때문에 반복문 위에 설정
         
         for(int i = 0 ; i<N; i++){
-            int count = 0 ;
-            
-            for(int j = 0 ; j<stages.length ; j++){
-                if(stages[j] == i + 1){
+            int count = 0; // 실패한 사람수 
+            for(int j = 0 ; j<stages.length; j++){
+                if(i + 1 == stages[j]){ //스테이지는 1부터 시작하기 때문에
                     count++;
-                    }
                 }
-            
-            arr[i] = (double)count / (double)len;
-            if(len == 0){
-                arr[i] = 0.0;
             }
-            copy[i] = arr[i];
-            len -= count; 
+            arr[i] = (double)count / (double)len ;
+            
+            if(len == 0){
+                arr[i] = 0.0; // 실패한 사람이 없을 경우의수 
+            }
+            copy[i] = arr[i];  // 순서가 중요한데 밑에서 arr로 sort해서 실패율 높을걸 따져야기때문에 arr 값을 복사해준다 
+            //조건이 //만약 실패율이 같은 스테이지가 있다면 작은 번호의 스테이지가 먼저 오도록 하면 된다 이기 때문
+            len -= count;
             num[i] = i + 1;
         }
         
         Arrays.sort(arr);
+        //Arrays.sort(arr, Collections.reverseOrder()); -> 사용 불가능 Integer or Double 이렇게 사용해야함 그래서 int [] num 을 만든거임 
+         
+        boolean [] check = new boolean [N]; // 실패율이 같을때 이미 체크 된건 제외해야해서 
         
-        boolean [] check = new boolean [N];
+        int index = 0 ;
         
-    
-        for(int i = 0 ; i<arr.length ; i++){
-            System.out.println(arr[i]);
-        }
-        
-        int index = 0;
-        
-        for(int i = N - 1 ; i>=0; i--){
-            for(int j = 0 ; j<copy.length; j++){
+        for(int i = N -1; i>=0; i--){ // arr 내림차순이 아니라 N - 1 부터 해주고 
+            for(int j = 0 ; j<copy.length; j++){  // 스테이지가 낮은거부터 찾아야해서 
                 if(arr[i] == copy[j] && !check[j]){
-                    answer[index] = num[j];
+                    answer[index++] = num[j];
                     check[j] = true;
-                    index++;
                     break;
                 }
             }
