2bcaee82b91c7afd05c3e9b208a58c8f446a85c8
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-09-24 17:56:48 +0900

    [level 2] Title: 괄호 회전하기, Time: 481.90 ms, Memory: 403 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,55 +1,55 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     public int solution(String s) {
         int answer = 0;
         
-        Stack<Character>stack = new Stack<>();
-        
         for(int i = 0 ; i<s.length(); i++){
             String str = "";
-            stack = new Stack<>();
+            Stack<Character>stack = new Stack<>();
             
-           for(int j = i ;j< i + s.length() ; j++){
-                str += s.charAt(j % s.length());
+            for(int j = i; j< i + s.length(); j++){
+                str += s.charAt(j %s.length());
             }
             
             for(int j = 0 ; j<str.length(); j++){
                 char word = str.charAt(j);
                 if(stack.isEmpty()){
-                    stack.add(word);
+                    stack.push(word);
                 }
-                else if(stack.peek() == '{'){
-                    if(word == '}'){
-                        stack.pop();
+                else{
+                    if(stack.peek() == '('){
+                        if(word == ')'){
+                            stack.pop();
+                        }
+                        else{
+                            stack.push(word);
+                        }
                     }
-                    else {
-                        stack.add(word);
+                    else if(stack.peek() == '['){
+                        if(word == ']'){
+                            stack.pop();
+                        }
+                        else{
+                            stack.push(word);
+                        }
                     }
-                }
-                else if(stack.peek() == '['){
-                    if(word == ']'){
-                        stack.pop();
+                    else if(stack.peek() == '{'){
+                        if(word == '}'){
+                            stack.pop();
+                        }
+                        else{
+                            stack.push(word);
+                        }
                     }
-                    else {
-                        stack.add(word);
-                    }
-                }
-                else if(stack.peek() == '('){
-                    if(word == ')'){
-                        stack.pop();
-                    }
-                    else {
-                        stack.add(word);
+                    else{
+                        stack.push(word);
                     }
                 }
             }
-            
-            if(stack.size() == 0){
+            if(stack.isEmpty()){
                 answer++;
             }
-            
         }
         return answer;
     }
