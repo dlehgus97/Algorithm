70f7175cf17ec6bc33047984299fc6f544aa963c
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-09-24 17:40:32 +0900

    [level 2] Title: 올바른 괄호, Time: 19.91 ms, Memory: 53.6 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,26 +1,27 @@
-import java.io.*;
 import java.util.*;
 
 class Solution {
     boolean solution(String s) {
         boolean answer = true;
+    
         Stack<String>stack = new Stack<>();
         
         for(int i = 0 ; i<s.length(); i++){
             if(s.charAt(i) == '('){
                 stack.push("(");
             }
-            else if(!stack.isEmpty() && s.charAt(i) == ')'){
+            else if(!stack.isEmpty() && s.charAt(i) == ')' && stack.peek() == "("){
                 stack.pop();
             }
             else if(stack.isEmpty() && s.charAt(i) == ')'){
                 stack.push(")");
-            }
+            } 
         }
+    
         if(!stack.isEmpty()){
-            answer = false ;
+            answer = false;
         }
-
+        
         return answer;
     }
 }
