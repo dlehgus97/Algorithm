ccbd71c11ded6c2dc07fd2be6f5369b830739d00
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-09-25 18:31:59 +0900

    [level 2] Title: 주식가격, Time: 88.54 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,14 +4,14 @@ class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int [prices.length];
         
-        for(int i = 0 ; i<prices.length - 1; i++){
+        for(int i = 0; i<prices.length; i++){
             Stack<Integer>stack = new Stack<>();
-            for(int j = i + 1; j<prices.length; j++){
-                if(prices[i]<=prices[j]){
-                    stack.add(prices[j]);
+            for(int j = i+1; j<prices.length; j++){
+                if(prices[i]<=prices[j]){ // 주식가격이 크거나 같을 때 스택에 담는다 .
+                    stack.push(prices[j]);
                 }
-                else {
-                    stack.add(1);
+                else{
+                    stack.push(1);
                     break;
                 }
             }
