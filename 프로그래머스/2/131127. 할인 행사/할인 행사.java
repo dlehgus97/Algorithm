3420da9852bee9d9e5ee6eac0e50660c741e9540
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-09-27 19:01:05 +0900

    [level 2] Title: 할인 행사, Time: 93.41 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,23 +1,30 @@
+import java.util.*;
+
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
         
-        int [] arr = new int [want.length];
+        HashMap<String,Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i<want.length; i++){
+            map.put(want[i],number[i]);
+        }
         
         for(int i = 0 ; i<discount.length - 9 ; i++){
-            arr = new int [want.length];
+            HashMap<String, Integer> diff_map = new HashMap<>();
             for(int j = i ; j<i+10; j++){
-                for(int k = 0 ; k<want.length; k++){
-                    if(want[k].equals(discount[j])){
-                        arr[k]++;
-                        break;
-                    }
+                if(diff_map.containsKey(discount[j])){
+                    diff_map.put(discount[j],diff_map.get(discount[j]) + 1);
+                }
+                else{
+                    diff_map.put(discount[j],1);
                 }
             }
+            
             boolean check = true;
             
             for(int j = 0 ; j<want.length; j++){
-                if(arr[j] != number[j]){
+                if(map.get(want[j]) != diff_map.get(want[j])){
                     check = false;
                     break;
                 }   
@@ -27,8 +34,6 @@ class Solution {
                 System.out.println(i);
             }
         }
-        
-        
         return answer;
     }
 }
