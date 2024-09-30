e45c4408bfe3ad8cbb8fdd0c49b0fca4d1bd2bc0
이진우 <76528931+dlehgus97@users.noreply.github.com>
2024-09-30 23:16:18 +0900

    [level 2] Title: 오픈채팅방, Time: 226.45 ms, Memory: 150 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.java
@@ -1,16 +1,14 @@
 import java.util.*;
 
 class Solution {
-    public ArrayList<String> solution(String[] record) {
+    public String[] solution(String[] record) {
         
-        ArrayList <String> list = new ArrayList<>();
+        ArrayList<String>list = new ArrayList<>();
         
-        
-        HashMap<String,String>map = new HashMap<>();
+        HashMap<String,String> map = new HashMap<>();
         
         for(int i = 0 ; i<record.length; i++){
             String [] arr = record[i].split(" ");
-            
             if(!arr[0].equals("Leave")){
                 map.put(arr[1],arr[2]);
             }
@@ -18,15 +16,20 @@ class Solution {
         
         for(int i = 0 ; i<record.length; i++){
             String [] arr = record[i].split(" ");
-            
             if(arr[0].equals("Enter")){
                 list.add(map.get(arr[1]) + "님이 들어왔습니다.");
             }
-                
             else if(arr[0].equals("Leave")){
                 list.add(map.get(arr[1]) + "님이 나갔습니다.");
             }
         }
-        return list;
+        
+        String[] answer = new String[list.size()];
+        int idx =  0;
+        
+        for(String s : list){
+            answer[idx++] = s;
+        }
+        return answer;
     }
 }
